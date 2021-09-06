6079c9ce7b0692307aea9331ecedf80cab8c8b90
ion petropoulos <ion.petropoulos@kariera.gr>
2021-09-06 19:58:02 +0300

    Add sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 const path = require('path');
-const withPlugins = require('next-compose-plugins');
+const { withSentryConfig } = require('@sentry/nextjs');
 
-module.exports = withPlugins([], {
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+const moduleExports = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
@@ -20,4 +32,6 @@ module.exports = withPlugins([], {
     });
     return config;
   },
-});
+};
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
