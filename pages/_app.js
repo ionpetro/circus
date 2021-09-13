fcd782cbb275161b98493b8fdb3e68217bae456d
ion petropoulos <ion.petropoulos@kariera.gr>
2021-09-13 21:50:31 +0300

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,25 @@ import UserContext from '../contexts/UserContext';
 import { useEffect, useState } from 'react';
 import Cookie from 'js-cookie';
 import axiosInstance from '../utils/http-client';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  const handleRouteChange = (url) => {
+    window.gtag('config', 'G-K234WBV32H', {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   const [user, setUser] = useState(null);
 
   const setUserData = async (token) => {
@@ -33,9 +50,26 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      <Component {...pageProps} />
-    </UserContext.Provider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-K234WBV32H"
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-K234WBV32H', { page_path: window.location.pathname });
+            `,
+        }}
+      />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Component {...pageProps} />
+      </UserContext.Provider>
+    </>
   );
 }
 
