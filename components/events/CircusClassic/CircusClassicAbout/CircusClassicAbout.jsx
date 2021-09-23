fa81440066fa5755d6288d9c0eba8f009dcdcf33
ion petropoulos <ion.petropoulos@kariera.gr>
2021-09-24 02:54:11 +0300

    Add circus classic event

---
+++ components/events/CircusClassic/CircusClassicAbout/CircusClassicAbout.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const CircusClassicAbout = () => {
+  return (
+    <>
+      <h4>ABOUT</h4>
+      <p>
+        Circus Strongman Experience organizes the first Circus Classic Event in
+        Greece. The Circus Classic is a two-day Strongman Event which will take
+        place in its wonderful space:{' '}
+        <a href={'https://bit.ly/37Adqw0'} target={'_blank'} rel={'noreferrer'}>
+          Connect Your City Training Center
+        </a>{' '}
+        in collaboration with{' '}
+        <a href={'https://bit.ly/2X9B93Y'} target={'_blank'} rel={'noreferrer'}>
+          IASIS
+        </a>{' '}
+        with the support of the{' '}
+        <a
+          href={'https://www.facebook.com/hellenicpowerlifting'}
+          target={'_blank'}
+          rel={'noreferrer'}
+        >
+          Hellenic Dynamic Triathlon Association
+        </a>
+        . The Event will include 6 events, which will be divided into 3 per day.
+      </p>
+    </>
+  );
+};
+
+export default CircusClassicAbout;
