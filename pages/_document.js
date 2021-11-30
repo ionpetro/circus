0eb958cfa3c4ae2fea8a8d2403ac90f81baf46ec
ion petropoulos <ion.petropoulos@kariera.gr>
2021-12-01 00:57:24 +0200

    Change favicon

--- pages/_document.js
+++ pages/_document.js
@@ -11,11 +11,31 @@ class MyDocument extends Document {
       <Html lang={'en'}>
         <Head>
           <link
-            rel={'shortcut icon'}
-            href={
-              'https://res.cloudinary.com/ionpetro/image/upload/v1627764951/circus_lsu3u9.png'
-            }
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/assets/favicon/apple-touch-icon.png"
           />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/assets/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/assets/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/assets/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
