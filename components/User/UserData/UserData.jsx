1fa16d3f9df8f8571a6593403a27ab8c891c9865
Ion Petro <ionpetro@gmail.com>
2024-09-10 04:32:15 +0300

    Create barchart

--- components/User/UserData/UserData.jsx
+++ components/User/UserData/UserData.jsx
@@ -1,37 +1,78 @@
 import {
-  LineChart,
+  BarChart,
   CartesianGrid,
   XAxis,
   YAxis,
   Tooltip,
-  Line,
+  Bar,
   ResponsiveContainer,
 } from 'recharts';
-import { transformDateShort } from '../../../utils/utilities';
+import styles from './UserData.module.scss';
+import UserContext from '../../../contexts/UserContext';
+import { useContext } from 'react';
+import UiSelect from '../../Ui/UiSelect/UiSelect';
 
-const UserData = ({ records, events }) => {
-  console.log(events);
-  const formatedRecords = records.map((event) => ({
-    date: transformDateShort(event.created_at),
-    score: event.score,
+const UserData = ({ records, events, event, setEvent }) => {
+  const { user } = useContext(UserContext);
+
+  const onEventChange = (e) => {
+    setEvent(findEvent(e.target.value));
+  };
+
+  const findEvent = (eventTitle) => {
+    const e = events.find((option) => option.title === eventTitle);
+    return e ? e : null;
+  };
+
+  const latestRecords = Array.from(
+    records
+      .reduce((map, obj) => {
+        const userId = obj.user.id;
+        const existingObj = map.get(userId);
+
+        // Compare and keep the latest `created_at`
+        if (
+          !existingObj ||
+          new Date(obj.created_at) > new Date(existingObj.created_at)
+        ) {
+          map.set(userId, obj);
+        }
+
+        return map;
+      }, new Map())
+      .values()
+  ).map((record) => ({
+    // map to a data format
+    user: record.user.username,
+    score: record.score,
+    fill: record.user.username === user?.username ? '#dd5828' : '#888888',
   }));
 
-  console.log(formatedRecords);
   return (
-    <ResponsiveContainer width={700} height={500}>
-      <LineChart
-        width={730}
-        height={250}
-        data={formatedRecords}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-      >
+    <>
+      {event && (
+        <UiSelect
+          className={styles.filter}
+          name={'event'}
+          options={events}
+          value={event.title}
+          onChange={onEventChange}
+        />
+      )}
+      <ResponsiveContainer width={700} height={500}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Line type="monotone" dataKey="score" stroke="#8884d8" />
-      </LineChart>
-    </ResponsiveContainer>
+
+        <BarChart width={730} height={250} data={latestRecords}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="user" />
+          <YAxis />
+          <Tooltip
+            contentStyle={{ backgroundColor: '#2f2f2f', color: 'white' }}
+          />
+          <Bar dataKey="score" fill="fill" />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
   );
 };
 
